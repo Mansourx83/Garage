e3dc8b218d2f60deb73943fecc8a099e772fae20
Youssef Mansour <yousaf83000@gmail.com>
2025-10-25 21:15:25 +0300

    employment supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:garage/features/admin/admin_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'features/auth/auth_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://muyfwnkayewwdghkfmbm.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11eWZ3bmtheWV3d2RnaGtmbWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MDYwNzksImV4cCI6MjA3Njk4MjA3OX0.XbIeVmfhumb0SQDD35URlOqYhlC0fHGNgFaUeG1YVZE',
+  );
   runApp(const MyApp());
 }
 
